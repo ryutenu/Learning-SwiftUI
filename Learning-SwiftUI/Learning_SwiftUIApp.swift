07ab692d4359f8ff1cbbc59183ef3a95aa36d4e9
Alan Liu <ryutenu.tokyo@gmail.com>
2023-04-18 23:34:08 +0900

    [Add] Organize files into folders

--- Learning-SwiftUI/Learning_SwiftUIApp.swift
+++ Learning-SwiftUI/Learning_SwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct Learning_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ Learning-SwiftUI/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  Learning-SwiftUI
+//
+//  Created by Alan Liu on 2023/04/18.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
