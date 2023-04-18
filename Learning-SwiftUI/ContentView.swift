8141347347783babd4fab6462adf9d6a8fa0cdba
Alan Liu <ryutenu.tokyo@gmail.com>
2023-04-18 23:27:20 +0900

    Initial Commit

---
+++ Learning-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Learning-SwiftUI
+//
+//  Created by Alan Liu on 2023/04/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning-SwiftUI/Learning_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_SwiftUIApp.swift
+//  Learning-SwiftUI
+//
+//  Created by Alan Liu on 2023/04/18.
+//
+
+import SwiftUI
+
+@main
+struct Learning_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
