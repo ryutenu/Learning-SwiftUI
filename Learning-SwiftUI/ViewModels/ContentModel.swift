c31f94c4986e69e94d8b9fbae0775d3ea9d34012
Alan Liu <ryutenu.tokyo@gmail.com>
2023-04-20 00:31:52 +0900

    [Mod] - Add assets - Add data files - Add models - Parse data in ContentModel

--- Learning-SwiftUI/ViewModels/ContentModel.swift
+++ Learning-SwiftUI/ViewModels/ContentModel.swift
@@ -9,4 +9,36 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        // Data
+        guard let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json") else { return }
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl)
+            let decoder = JSONDecoder()
+            let modules = try decoder.decode([Module].self, from: jsonData)
+            self.modules = modules
+        }
+        catch {
+            print("Couldn't parse local data")
+        }
+        
+        // Style
+        guard let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html") else { return }
+        
+        do {
+            styleData = try Data(contentsOf: styleUrl)
+        }
+        catch {
+            print("Couldn't parse local data")
+        }
+    }
 }
